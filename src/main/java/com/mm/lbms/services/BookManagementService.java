a5117ff768b92ca8e5b8dcab98a0ba2e1c786419
AungKoLin828 <aungko.linn404@gmail.com>
2024-04-15 01:38:56 +0630

    update sources

--- src/main/java/com/mm/lbms/services/BookManagementService.java
+++ src/main/java/com/mm/lbms/services/BookManagementService.java
@@ -1,5 +1,24 @@
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-14
+**************************/
 package com.mm.lbms.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.mm.lbms.dto.BookDTO;
+
 public interface BookManagementService {
+	
+	BookDTO saveBook(BookDTO bookDto);
+	
+	List<BookDTO> findAll();
+	
+	Optional<BookDTO> findOne(Long id);
+	
+	Optional<BookDTO> partialUpdate(BookDTO bookDto);
+	
+	void deleteBook(Long id);
 
 }
--- src/main/java/com/mm/lbms/services/PatronsManagementService.java
+++ src/main/java/com/mm/lbms/services/PatronsManagementService.java
@@ -1,5 +1,23 @@
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-14
+**************************/
 package com.mm.lbms.services;
+import java.util.List;
+import java.util.Optional;
+
+import com.mm.lbms.dto.PatronsDTO;
 
 public interface PatronsManagementService {
+	
+	PatronsDTO savePatrons(PatronsDTO patronsDto);
+	
+	List<PatronsDTO> findAll();
+	
+	Optional<PatronsDTO> findOne(Long id);
+	
+	Optional<PatronsDTO> partialUpdate(PatronsDTO patronsDto);
+	
+	void deletePatrons(Long id);
 
 }
