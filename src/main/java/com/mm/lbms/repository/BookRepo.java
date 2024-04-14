304141927d9b77e3e7e6600ea65060a4f5d58441
AungKoLin828 <aungko.linn404@gmail.com>
2024-04-15 01:39:50 +0630

    update sources

--- src/main/java/com/mm/lbms/repository/BookRepo.java
+++ src/main/java/com/mm/lbms/repository/BookRepo.java
@@ -1,9 +1,14 @@
 package com.mm.lbms.repository;
-
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-14
+**************************/
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.mm.lbms.domain.Book;
+
 @Repository
-public interface Book extends JpaRepository<Book, Long> {
+public interface BookRepo extends JpaRepository<Book, Long> {
 
 }
--- src/main/java/com/mm/lbms/repository/PatronsRepo.java
+++ src/main/java/com/mm/lbms/repository/PatronsRepo.java
@@ -1,9 +1,12 @@
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-14
+**************************/
 package com.mm.lbms.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Patrons extends JpaRepository<Patrons, Long> {
+public interface PatronsRepo extends JpaRepository<PatronsRepo, Long> {
 
 }
