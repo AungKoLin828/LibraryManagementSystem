a2f6f8d1c9e34504bb9e2d911a745938a35f9c11
AungKoLin828 <aungko.linn404@gmail.com>
2024-04-15 01:39:19 +0630

    update sources

--- src/main/java/com/mm/lbms/service/impl/BookManagementServiceImpl.java
+++ src/main/java/com/mm/lbms/service/impl/BookManagementServiceImpl.java
@@ -1,10 +1,79 @@
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-14
+**************************/
 package com.mm.lbms.service.impl;
-
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
+import com.mm.lbms.domain.Book;
+import com.mm.lbms.dto.BookDTO;
+import com.mm.lbms.mapper.BookMapper;
+import com.mm.lbms.repository.BookRepo;
 import com.mm.lbms.services.BookManagementService;
 
 @Service
 public class BookManagementServiceImpl implements BookManagementService{
+	
+	private final BookMapper bookMapper;
+	
+	private final BookRepo bookRepo;
+	
+	public BookManagementServiceImpl(BookMapper bookMapper,BookRepo bookRepo) {
+		this.bookMapper = bookMapper;
+		this.bookRepo = bookRepo;
+	}
+	
+	@Override
+	public BookDTO saveBook(BookDTO bookDto) {
+		// TODO Auto-generated method stub
+		// Mapping DTO to entity
+		Book book =  bookMapper.toEntity(bookDto);
+		// Saving entity
+		book = bookRepo.save(book);
+		// Mapping saved entity back to DTO
+		return bookMapper.toDto(book);
+	}
+
+	@Override
+	public List<BookDTO> findAll() {
+		// TODO Auto-generated method stub	
+		// Returning list of all books
+		  return bookRepo.findAll().stream().map(bookMapper::toDto)
+		  .collect(Collectors.toCollection(LinkedList::new));
+		 
+	}
+
+	@Override
+	public Optional<BookDTO> findOne(Long id) {
+		// TODO Auto-generated method stub
+		// Returning book by id if found
+		return bookRepo.findById(id).map(bookMapper::toDto);
+	}
+
+	@Override
+	public Optional<BookDTO> partialUpdate(BookDTO bookDto) {
+		// TODO Auto-generated method stub
+		// Partial update of book by id
+		return bookRepo.findById(bookDto.getId())
+				.map(exctBook -> {
+					// Mapping DTO properties to existing entity
+					bookMapper.partialUpdate(exctBook, bookDto);
+					return exctBook;
+				}
+			)
+			// Saving updated entity
+			.map(bookRepo::save)
+			.map(bookMapper::toDto);
+	}
+
+	@Override
+	public void deleteBook(Long id) {
+		// TODO Auto-generated method stub
+		bookRepo.deleteById(id);
+	}
 
 }
--- src/main/java/com/mm/lbms/service/impl/PatronsManagementServiceImpl.java
+++ src/main/java/com/mm/lbms/service/impl/PatronsManagementServiceImpl.java
@@ -1,10 +1,46 @@
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-13
+**************************/
 package com.mm.lbms.service.impl;
-
+import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
+import com.mm.lbms.dto.PatronsDTO;
 import com.mm.lbms.services.PatronsManagementService;
 
 @Service
 public class PatronsManagementServiceImpl implements PatronsManagementService{
+
+	@Override
+	public PatronsDTO savePatrons(PatronsDTO patronsDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<PatronsDTO> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<PatronsDTO> findOne(Long id) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<PatronsDTO> partialUpdate(PatronsDTO patronsDto) {
+		// TODO Auto-generated method stub
+		return Optional.empty();
+	}
+
+	@Override
+	public void deletePatrons(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
