fce2f8736883c77ed936d44d2cd4dae76fc216b6
AungKoLin828 <aungko.linn404@gmail.com>
2024-04-15 01:40:38 +0630

    add and update sources

--- src/main/java/com/mm/lbms/dto/PatronsDTO.java
+++ src/main/java/com/mm/lbms/dto/PatronsDTO.java
@@ -1,14 +1,65 @@
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-14
+**************************/
 package com.mm.lbms.dto;
-
 import java.io.Serializable;
 
-public class PatronsDTO implements Serializable{
+public class PatronsDTO implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long Id;
+
+	private String name;
+
+	private String phoneNo;
+
+	private String email;
+
+	private String address;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
 }
