0a974be6784f931690c2df140e7b23bada365bc2
AungKoLin828 <aungko.linn404@gmail.com>
2024-04-15 01:41:44 +0630

    update sources

--- src/main/java/com/mm/lbms/domain/Book.java
+++ src/main/java/com/mm/lbms/domain/Book.java
@@ -6,19 +6,74 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
+
+import jakarta.validation.constraints.NotNull;
+
 import javax.persistence.Id;
 
 @Entity
-@Table(name="book_tbl")
-public class Book implements Serializable{
+@Table(name = "book_tbl")
+public class Book implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "equenceGenerator")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "equenceGenerator")
 	private Long Id;
+
+	@NotNull
+	private String title;
+
+	@NotNull
+	private String author;
+
+	@NotNull
+	private int publicationYear;
+
+	@NotNull
+	private String isbn;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
 }
