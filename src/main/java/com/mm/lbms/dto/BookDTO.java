bdb3631737512e75ca4b0f894a55b538f154a18a
AungKoLin828 <aungko.linn404@gmail.com>
2024-04-15 01:40:56 +0630

    add and update sources

--- src/main/java/com/mm/lbms/dto/BookDTO.java
+++ src/main/java/com/mm/lbms/dto/BookDTO.java
@@ -1,14 +1,67 @@
 package com.mm.lbms.dto;
 
+/**************************
+*Author Name   : Aung Ko Lin
+*Creation Date : 2024-04-13
+**************************/
+
 import java.io.Serializable;
 
-public class BookDTO implements Serializable{
+public class BookDTO implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long Id;
+
+	private String title;
+
+	private String author;
+
+	private int publicationYear;
+
+	private String isbn;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
 }
